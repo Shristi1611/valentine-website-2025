0439f6d466399fef34eb6fe99b7404265ba8f77b
Shristi1611 <shristisinha59@gmail.com>
2025-02-13 23:35:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Mere Valentine banoge, Jaaniiiiii? 🖤🖤",
+    pageTitle: "Mere Valentine banoge, Jaaniiiiiiiii? 🖤🖤",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
